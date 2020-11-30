60c6e8f70c6ef40cc1dd4bf590412fcf5b67fc56
Mahmoud-Nasser01 <m.nasser.fulltact@gmail.com>
2020-11-30 15:07:58 +0200

    initial comit

--- Json-Codable/ViewController.swift
+++ Json-Codable/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var petetions = [Petition]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petetions.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            cell.textLabel?.text = "pettion title here"
+            cell.detailTextLabel?.text = "petetion details goes here"
+            return cell
+        
+    }
+    
 }
 
